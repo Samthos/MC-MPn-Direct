ec49af686ef96f1d1dea5217bde14d019585e97b
Alexander Doran <alexdor@gmail.com>
2018-06-14 22:31:18 -0500

    ik and il strength reduction

--- src/MCMP/qc_mcmp4.cpp
+++ src/MCMP/qc_mcmp4.cpp
@@ -6,198 +6,170 @@
 void MP::mcmp4_energy(double& emp4, std::vector<double>& control) {
   emp4 = 0.0;
   control[0] = 0.0;
-  /*
-  for (auto it = 0; it < iops.iopns[KEYS::MC_NPAIR]; it++) {
-    double en_jkl = 0;
-    double ct_jkl = 0;
-    for (auto jt = 0; jt < iops.iopns[KEYS::MC_NPAIR]; jt++) {
-      if (it == jt) continue;
-      auto ij = it * iops.iopns[KEYS::MC_NPAIR] + jt;
 
-      double en_kl = 0;
-      double ct_kl = 0;
-      for (auto kt = 0; kt < iops.iopns[KEYS::MC_NPAIR]; kt++) {
-        if (it == kt || jt == kt) continue;
-        auto ik = it * iops.iopns[KEYS::MC_NPAIR] + kt;
-        auto jk = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
-
-        double en_l = 0;
-        double ct_l = 0;
-        for (auto lt = 0; lt < iops.iopns[KEYS::MC_NPAIR]; lt++) {
-          if (it == lt || jt == lt || kt == lt) continue;
-          auto il = it * iops.iopns[KEYS::MC_NPAIR] + lt;
-          auto jl = jt * iops.iopns[KEYS::MC_NPAIR] + lt;
-          auto kl = kt * iops.iopns[KEYS::MC_NPAIR] + lt;
-
-          double en = 0;
-
-          #include "qc_mcmp4_ijkl_org.h"
-
-          en_l += en * el_pair_list[lt].rv;
-          ct_l += en / el_pair_list[lt].wgt;
-        }
-        en_kl += en_l * el_pair_list[kt].rv;
-        ct_kl += ct_l / el_pair_list[kt].wgt;
-      }
-      ct_jkl += ct_kl / el_pair_list[jt].wgt;
-      en_jkl += en_kl * el_pair_list[jt].rv;
-    }
-    control[0] -= ct_jkl / el_pair_list[it].wgt;
-    emp4       -= en_jkl * el_pair_list[it].rv;
-  }
-  */
-
-  for (auto it = 0; it < iops.iopns[KEYS::MC_NPAIR]; it++) {
-    double en_jkl = 0;
-    double ct_jkl = 0;
-    for (auto jt = 0; jt < iops.iopns[KEYS::MC_NPAIR]; jt++) {
-      if (it == jt) continue;
-      auto ij = it * iops.iopns[KEYS::MC_NPAIR] + jt;
-
-      double en_kl = 0;
-      double ct_kl = 0;
-      for (auto kt = 0; kt < iops.iopns[KEYS::MC_NPAIR]; kt++) {
-        if (it == kt || jt == kt) continue;
-        auto ik = it * iops.iopns[KEYS::MC_NPAIR] + kt;
-        auto jk = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
-
-        double en_l = 0;
-        double ct_l = 0;
-        for (auto lt = 0; lt < iops.iopns[KEYS::MC_NPAIR]; lt++) {
-          if (it == lt || jt == lt || kt == lt) continue;
-          auto il = it * iops.iopns[KEYS::MC_NPAIR] + lt;
-          auto jl = jt * iops.iopns[KEYS::MC_NPAIR] + lt;
-          auto kl = kt * iops.iopns[KEYS::MC_NPAIR] + lt;
-
-          double en = 0;
-
-// #include "qc_mcmp4_ijkl_ij.h"
-#include "qc_mcmp4_ijkl_ik.h"
-#include "qc_mcmp4_ijkl_il.h"
-
-          en_l += en * el_pair_list[lt].rv;
-          ct_l += en / el_pair_list[lt].wgt;
-        }
-        en_kl += en_l * el_pair_list[kt].rv;
-        ct_kl += ct_l / el_pair_list[kt].wgt;
-      }
-      ct_jkl += ct_kl / el_pair_list[jt].wgt;
-      en_jkl += en_kl * el_pair_list[jt].rv;
-    }
-    control[0] += ct_jkl / el_pair_list[it].wgt;
-    emp4       += en_jkl * el_pair_list[it].rv;
-  }
-
-
+  // ij contracted sums
   for (auto it = 0; it < iops.iopns[KEYS::MC_NPAIR]; it++) {
     double en_i = 0;
     double ct_i = 0;
     for (auto jt = 0; jt < iops.iopns[KEYS::MC_NPAIR]; jt++) {
       if (it == jt) continue;
-      auto ij = it * iops.iopns[KEYS::MC_NPAIR] + jt;
-      std::array<double, 36> en_k, ct_k;
-      en_k.fill(0); ct_k.fill(0);
+      auto it_jt = it * iops.iopns[KEYS::MC_NPAIR] + jt;
+      std::array<double, 36> en_kt, ct_kt;
+      en_kt.fill(0); ct_kt.fill(0);
       for (auto kt = 0; kt < iops.iopns[KEYS::MC_NPAIR]; kt++) {
         if (it == kt || jt == kt) continue;
-        auto ik = it * iops.iopns[KEYS::MC_NPAIR] + kt;
-        auto jk = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
+        auto it_kt = it * iops.iopns[KEYS::MC_NPAIR] + kt;
+        auto jt_kt = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
 
         std::array<double, 36> en;
 #include "qc_mcmp4_ij_k.h"
-        std::transform(en_k.begin(), en_k.end(), en.begin(), en_k.begin(), [&](double x, double y) {return x + y * el_pair_list[kt].rv;});
-        std::transform(ct_k.begin(), ct_k.end(), en.begin(), ct_k.begin(), [&](double x, double y) {return x + y / el_pair_list[kt].wgt;});
+        std::transform(en_kt.begin(), en_kt.end(), en.begin(), en_kt.begin(), [&](double x, double y) {return x + y * el_pair_list[kt].rv;});
+        std::transform(ct_kt.begin(), ct_kt.end(), en.begin(), ct_kt.begin(), [&](double x, double y) {return x + y / el_pair_list[kt].wgt;});
       }
 
-      std::array<double, 36> en_l, ct_l;
-      en_l.fill(0); ct_l.fill(0);
+      std::array<double, 36> en_lt, ct_lt;
+      en_lt.fill(0); ct_lt.fill(0);
       for (auto lt = 0; lt < iops.iopns[KEYS::MC_NPAIR]; lt++) {
         if (it == lt || jt == lt) continue;
-        auto il = it * iops.iopns[KEYS::MC_NPAIR] + lt;
-        auto jl = jt * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto it_lt = it * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto jt_lt = jt * iops.iopns[KEYS::MC_NPAIR] + lt;
 
         std::array<double, 36> en;
 #include "qc_mcmp4_ij_l.h"
-        std::transform(en_l.begin(), en_l.end(), en.begin(), en_l.begin(), [&](double x, double y) {return x + y * el_pair_list[lt].rv;});
-        std::transform(ct_l.begin(), ct_l.end(), en.begin(), ct_l.begin(), [&](double x, double y) {return x + y / el_pair_list[lt].wgt;});
+        std::transform(en_lt.begin(), en_lt.end(), en.begin(), en_lt.begin(), [&](double x, double y) {return x + y * el_pair_list[lt].rv;});
+        std::transform(ct_lt.begin(), ct_lt.end(), en.begin(), ct_lt.begin(), [&](double x, double y) {return x + y / el_pair_list[lt].wgt;});
       }
 
       double en_corr = 0;
       double ct_corr = 0;
       for (auto kt = 0, lt = 0; kt < iops.iopns[KEYS::MC_NPAIR]; kt++, lt++) {
         if (it == kt || jt == kt) continue;
-        auto ik = it * iops.iopns[KEYS::MC_NPAIR] + kt;
-        auto jk = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
-        auto il = it * iops.iopns[KEYS::MC_NPAIR] + lt;
-        auto jl = jt * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto it_kt = it * iops.iopns[KEYS::MC_NPAIR] + kt;
+        auto jt_kt = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
+        auto it_lt = it * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto jt_lt = jt * iops.iopns[KEYS::MC_NPAIR] + lt;
         double en = 0;
 #include "qc_mcmp4_ij.h"
         en_corr += en * el_pair_list[kt].rv * el_pair_list[lt].rv;
         ct_corr += en / el_pair_list[kt].wgt / el_pair_list[lt].wgt;
       }
 
-      en_i += std::inner_product(en_k.begin(), en_k.end(), en_l.begin(), -en_corr) * el_pair_list[jt].rv;
-      ct_i += std::inner_product(ct_k.begin(), ct_k.end(), ct_l.begin(), -ct_corr) / el_pair_list[jt].wgt;
+      en_i += std::inner_product(en_kt.begin(), en_kt.end(), en_lt.begin(), -en_corr) * el_pair_list[jt].rv;
+      ct_i += std::inner_product(ct_kt.begin(), ct_kt.end(), ct_lt.begin(), -ct_corr) / el_pair_list[jt].wgt;
     }
     emp4 += en_i * el_pair_list[it].rv;
     control[0] += ct_i / el_pair_list[it].wgt;
   }
-  
-  /*
+
+  // ik contracted sums
   for (auto it = 0; it < iops.iopns[KEYS::MC_NPAIR]; it++) {
     double en_i = 0;
     double ct_i = 0;
     for (auto kt = 0; kt < iops.iopns[KEYS::MC_NPAIR]; kt++) {
       if (it == kt) continue;
-      auto ik = it * iops.iopns[KEYS::MC_NPAIR] + kt;
-
-      std::array<double, 36> en_j, ct_j;
-      en_j.fill(0); ct_j.fill(0);
+      auto it_kt = it * iops.iopns[KEYS::MC_NPAIR] + kt;
+      std::array<double, 36> en_jt, ct_jt;
+      en_jt.fill(0); ct_jt.fill(0);
       for (auto jt = 0; jt < iops.iopns[KEYS::MC_NPAIR]; jt++) {
-        if (it == jt || kt == jt) continue;
-        auto ij = it * iops.iopns[KEYS::MC_NPAIR] + jt;
-        auto jk = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
+        if (it == jt || jt == kt) continue;
+        auto it_jt = it * iops.iopns[KEYS::MC_NPAIR] + jt;
+        auto jt_kt = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
 
         std::array<double, 36> en;
 #include "qc_mcmp4_ik_j.h"
-        std::transform(en_j.begin(), en_j.end(), en.begin(), en_j.begin(), [&](double x, double y) {return x + y * el_pair_list[jt].rv;});
-        std::transform(ct_j.begin(), ct_j.end(), en.begin(), ct_j.begin(), [&](double x, double y) {return x + y / el_pair_list[jt].wgt;});
+        std::transform(en_jt.begin(), en_jt.end(), en.begin(), en_jt.begin(), [&](double x, double y) {return x + y * el_pair_list[jt].rv;});
+        std::transform(ct_jt.begin(), ct_jt.end(), en.begin(), ct_jt.begin(), [&](double x, double y) {return x + y / el_pair_list[jt].wgt;});
       }
 
-      std::array<double, 36> en_l, ct_l;
-      en_l.fill(0); ct_l.fill(0);
+      std::array<double, 36> en_lt, ct_lt;
+      en_lt.fill(0); ct_lt.fill(0);
       for (auto lt = 0; lt < iops.iopns[KEYS::MC_NPAIR]; lt++) {
         if (it == lt || kt == lt) continue;
-        auto il = it * iops.iopns[KEYS::MC_NPAIR] + lt;
-        auto kl = kt * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto it_lt = it * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto kt_lt = kt * iops.iopns[KEYS::MC_NPAIR] + lt;
 
         std::array<double, 36> en;
 #include "qc_mcmp4_ik_l.h"
-        std::transform(en_l.begin(), en_l.end(), en.begin(), en_l.begin(), [&](double x, double y) {return x + y * el_pair_list[lt].rv;});
-        std::transform(ct_l.begin(), ct_l.end(), en.begin(), ct_l.begin(), [&](double x, double y) {return x + y / el_pair_list[lt].wgt;});
+        std::transform(en_lt.begin(), en_lt.end(), en.begin(), en_lt.begin(), [&](double x, double y) {return x + y * el_pair_list[lt].rv;});
+        std::transform(ct_lt.begin(), ct_lt.end(), en.begin(), ct_lt.begin(), [&](double x, double y) {return x + y / el_pair_list[lt].wgt;});
       }
 
       double en_corr = 0;
       double ct_corr = 0;
       for (auto jt = 0, lt = 0; jt < iops.iopns[KEYS::MC_NPAIR]; jt++, lt++) {
         if (it == jt || kt == jt) continue;
-        auto ij = it * iops.iopns[KEYS::MC_NPAIR] + jt;
-        auto jk = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
-        auto il = it * iops.iopns[KEYS::MC_NPAIR] + lt;
-        auto kl = kt * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto it_jt = it * iops.iopns[KEYS::MC_NPAIR] + jt;
+        auto jt_kt = jt * iops.iopns[KEYS::MC_NPAIR] + kt;
+        auto it_lt = it * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto kt_lt = kt * iops.iopns[KEYS::MC_NPAIR] + lt;
         double en = 0;
 #include "qc_mcmp4_ik.h"
         en_corr += en * el_pair_list[jt].rv * el_pair_list[lt].rv;
         ct_corr += en / el_pair_list[jt].wgt / el_pair_list[lt].wgt;
       }
 
-      en_i += std::inner_product(en_j.begin(), en_j.end(), en_l.begin(), -en_corr) * el_pair_list[kt].rv;
-      ct_i += std::inner_product(ct_j.begin(), ct_j.end(), ct_l.begin(), -ct_corr) / el_pair_list[kt].wgt;
+      en_i += std::inner_product(en_jt.begin(), en_jt.end(), en_lt.begin(), -en_corr) * el_pair_list[kt].rv;
+      ct_i += std::inner_product(ct_jt.begin(), ct_jt.end(), ct_lt.begin(), -ct_corr) / el_pair_list[kt].wgt;
     }
     emp4 += en_i * el_pair_list[it].rv;
     control[0] += ct_i / el_pair_list[it].wgt;
   }
-  */
 
+  // il contracted sums
+  for (auto it = 0; it < iops.iopns[KEYS::MC_NPAIR]; it++) {
+    double en_i = 0;
+    double ct_i = 0;
+    for (auto lt = 0; lt < iops.iopns[KEYS::MC_NPAIR]; lt++) {
+      if (it == lt) continue;
+      auto it_lt = it * iops.iopns[KEYS::MC_NPAIR] + lt;
+      std::array<double, 36> en_kt, ct_kt;
+      en_kt.fill(0); ct_kt.fill(0);
+      for (auto kt = 0; kt < iops.iopns[KEYS::MC_NPAIR]; kt++) {
+        if (it == kt || lt == kt) continue;
+        auto it_kt = it * iops.iopns[KEYS::MC_NPAIR] + kt;
+        auto kt_lt = kt * iops.iopns[KEYS::MC_NPAIR] + lt;
+
+        std::array<double, 36> en;
+#include "qc_mcmp4_il_k.h"
+        std::transform(en_kt.begin(), en_kt.end(), en.begin(), en_kt.begin(), [&](double x, double y) {return x + y * el_pair_list[kt].rv;});
+        std::transform(ct_kt.begin(), ct_kt.end(), en.begin(), ct_kt.begin(), [&](double x, double y) {return x + y / el_pair_list[kt].wgt;});
+      }
+
+      std::array<double, 36> en_jt, ct_jt;
+      en_jt.fill(0); ct_jt.fill(0);
+      for (auto jt = 0; jt < iops.iopns[KEYS::MC_NPAIR]; jt++) {
+        if (it == jt || lt == jt) continue;
+        auto it_jt = it * iops.iopns[KEYS::MC_NPAIR] + jt;
+        auto jt_lt = jt * iops.iopns[KEYS::MC_NPAIR] + lt;
+
+        std::array<double, 36> en;
+#include "qc_mcmp4_il_j.h"
+        std::transform(en_jt.begin(), en_jt.end(), en.begin(), en_jt.begin(), [&](double x, double y) {return x + y * el_pair_list[jt].rv;});
+        std::transform(ct_jt.begin(), ct_jt.end(), en.begin(), ct_jt.begin(), [&](double x, double y) {return x + y / el_pair_list[jt].wgt;});
+      }
+
+      double en_corr = 0;
+      double ct_corr = 0;
+      for (auto kt = 0, jt = 0; kt < iops.iopns[KEYS::MC_NPAIR]; kt++, jt++) {
+        if (it == kt || lt == kt) continue;
+        auto it_kt = it * iops.iopns[KEYS::MC_NPAIR] + kt;
+        auto kt_lt = kt * iops.iopns[KEYS::MC_NPAIR] + lt;
+        auto it_jt = it * iops.iopns[KEYS::MC_NPAIR] + jt;
+        auto jt_lt = jt * iops.iopns[KEYS::MC_NPAIR] + lt;
+        double en = 0;
+#include "qc_mcmp4_il.h"
+        en_corr += en * el_pair_list[kt].rv * el_pair_list[jt].rv;
+        ct_corr += en / el_pair_list[kt].wgt / el_pair_list[jt].wgt;
+      }
+
+      en_i += std::inner_product(en_kt.begin(), en_kt.end(), en_jt.begin(), -en_corr) * el_pair_list[lt].rv;
+      ct_i += std::inner_product(ct_kt.begin(), ct_kt.end(), ct_jt.begin(), -ct_corr) / el_pair_list[lt].wgt;
+    }
+    emp4 += en_i * el_pair_list[it].rv;
+    control[0] += ct_i / el_pair_list[it].wgt;
+  }
+
+  // fourth order sums
   for (auto it = 0; it < iops.iopns[KEYS::MC_NPAIR]; it++) {
     double en_jkl = 0;
     double ct_jkl = 0;
