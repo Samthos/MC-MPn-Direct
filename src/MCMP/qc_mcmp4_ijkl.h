58f8222db3866e167f8636622425bb30a5c87941
Alexander Doran <aedoran2@illinois.edu>
2018-06-18 13:49:34 -0500

    double jl compression

--- src/MCMP/qc_mcmp4_ijkl.h
+++ src/MCMP/qc_mcmp4_ijkl.h
@@ -3,13 +3,10 @@ en[0]  = ovps.o_set[2][0].s_11[il] * ovps.o_set[2][0].s_22[il] * ovps.v_set[2][1
 
 en[1]  = ovps.o_set[2][0].s_12[il] * ovps.v_set[2][0].s_11[il] * ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][2].s_12[kl];
 en[5]  = ovps.o_set[2][0].s_12[il] * ovps.v_set[2][0].s_11[il] * ovps.v_set[2][1].s_12[jl] * ovps.o_set[2][2].s_11[kl];
-
 en[3]  = ovps.o_set[2][0].s_12[il] * ovps.v_set[2][0].s_21[il] * ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][2].s_12[kl];
 en[9]  = ovps.o_set[2][0].s_12[il] * ovps.v_set[2][0].s_21[il] * ovps.v_set[2][1].s_12[jl] * ovps.o_set[2][2].s_11[kl];
-
 en[2]  = ovps.o_set[2][0].s_12[il] * ovps.v_set[2][0].s_12[il] * ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][2].s_11[kl];
 en[7]  = ovps.o_set[2][0].s_12[il] * ovps.v_set[2][0].s_12[il] * ovps.v_set[2][1].s_11[jl] * ovps.o_set[2][2].s_11[kl];
-
 en[4]  = ovps.o_set[2][0].s_12[il] * ovps.v_set[2][0].s_22[il] * ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][2].s_11[kl];
 en[11] = ovps.o_set[2][0].s_12[il] * ovps.v_set[2][0].s_22[il] * ovps.v_set[2][1].s_11[jl] * ovps.o_set[2][2].s_11[kl];
 
@@ -20,18 +17,13 @@ en[13] = ovps.v_set[2][0].s_11[il] * ovps.v_set[2][0].s_22[il] * ovps.o_set[2][1
 en[14] = ovps.o_set[2][1].s_11[jl] * ovps.o_set[2][1].s_22[jl] * ovps.v_set[2][0].s_12[il] * ovps.v_set[2][2].s_11[kl];
 
 en[15]  = ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][1].s_11[jl] * ovps.o_set[2][0].s_12[il] * ovps.v_set[2][2].s_12[kl];
-en[16]  = ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][1].s_12[jl] * ovps.o_set[2][0].s_12[il] * ovps.v_set[2][2].s_11[kl];
-en[17]  = ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][1].s_21[jl] * ovps.o_set[2][0].s_12[il] * ovps.v_set[2][2].s_12[kl];
-en[18]  = ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][1].s_22[jl] * ovps.o_set[2][0].s_12[il] * ovps.v_set[2][2].s_11[kl];
-en[19] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_12[jl] * ovps.v_set[2][0].s_11[il] * ovps.o_set[2][2].s_11[kl];
-en[20] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_22[jl] * ovps.v_set[2][0].s_11[il] * ovps.o_set[2][2].s_11[kl];
 en[21] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_11[jl] * ovps.v_set[2][0].s_12[il] * ovps.o_set[2][2].s_11[kl];
+en[17]  = ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][1].s_21[jl] * ovps.o_set[2][0].s_12[il] * ovps.v_set[2][2].s_12[kl];
 en[22] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_21[jl] * ovps.v_set[2][0].s_12[il] * ovps.o_set[2][2].s_11[kl];
-en[23] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_12[jl] * ovps.v_set[2][0].s_21[il] * ovps.o_set[2][2].s_11[kl];
-en[24] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_22[jl] * ovps.v_set[2][0].s_21[il] * ovps.o_set[2][2].s_11[kl];
-en[25] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_11[jl] * ovps.v_set[2][0].s_22[il] * ovps.o_set[2][2].s_11[kl];
-en[26] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_21[jl] * ovps.v_set[2][0].s_22[il] * ovps.o_set[2][2].s_11[kl];
-
+en[16]  = ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][1].s_12[jl] * ovps.o_set[2][0].s_12[il] * ovps.v_set[2][2].s_11[kl];
+en[19] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_12[jl] * ovps.v_set[2][0].s_11[il] * ovps.o_set[2][2].s_11[kl];
+en[18]  = ovps.o_set[2][1].s_11[jl] * ovps.v_set[2][1].s_22[jl] * ovps.o_set[2][0].s_12[il] * ovps.v_set[2][2].s_11[kl];
+en[20] = ovps.o_set[2][1].s_12[jl] * ovps.v_set[2][1].s_22[jl] * ovps.v_set[2][0].s_11[il] * ovps.o_set[2][2].s_11[kl];
 en[27] = ovps.v_set[2][1].s_11[jl] * ovps.v_set[2][1].s_22[jl] * ovps.o_set[2][0].s_12[il] * ovps.o_set[2][2].s_11[kl];
 
 en[28] = ovps.o_set[2][2].s_11[kl] * ovps.o_set[2][2].s_22[kl] * ovps.v_set[2][0].s_11[il] * ovps.v_set[2][1].s_12[jl];
