b03382f18fb06f2a835db1405942a335dcd64ed6
Alexander <alexdor@gmail.com>
2020-03-24 16:42:07 -0500

    bug in waverfunction evaluation code

--- src/cpu_qc_monte.cpp
+++ src/cpu_qc_monte.cpp
@@ -5,9 +5,9 @@ void QC_monte::update_wavefunction() {
     auto code = it.first & WT::mask;
     switch (code) {
       case WT::normal: basis.host_psi_get(it.second, *it.second.pos); break;
-      case WT::dx: basis.host_psi_get(it.second, *it.second.pos); break;
-      case WT::dy: basis.host_psi_get(it.second, *it.second.pos); break;
-      case WT::dz: basis.host_psi_get(it.second, *it.second.pos); break;
+      case WT::dx: basis.host_psi_get_dx(it.second, *it.second.pos); break;
+      case WT::dy: basis.host_psi_get_dy(it.second, *it.second.pos); break;
+      case WT::dz: basis.host_psi_get_dz(it.second, *it.second.pos); break;
     }
   }
 }
