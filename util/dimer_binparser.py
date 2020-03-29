37af1517f7c8693bb1da678ed843332bea4bf792
hydrocodone <44106031+diracs-delta@users.noreply.github.com>
2020-03-29 10:21:00 -0500

    dimer_binparser: implement CV corrections
    this unfortunately does not replicate the MC-MP2 output. the energy and
    corrected energy converge after ~10^3 iterations, but differ initially.
    the relative error in corrected energy still differs from the MC-MP2
    output after ~10^5 iterations, and even more initially.
    
    I suspsect that solving linear systems is implementation/library
    dependent, and that some roundoff error is causing a discrepancy between
    the C++ MC-MP2 algorithm (which I believe uses the Armadillo linalg lib)
    and the binary parser using numpy.

--- util/dimer_binparser.py
+++ util/dimer_binparser.py
@@ -13,20 +13,31 @@ except:
     print("USAGE: dimer_binparser.py [emp binary file] [cv binary file]")
     sys.exit()
 
-emp_array = np.fromfile(emp_filename)
-cv_array = np.fromfile(cv_filename)
-cv_array = np.reshape(cv_array, (cv_array.size // 6, 6))
+emp = np.fromfile(emp_filename)
+cv = np.fromfile(cv_filename)
+cv = np.reshape(cv, (cv.size // 6, 6))
 
-print(emp_array)
-print(cv_array)
+steps = emp.size
+E_cv_cov = np.zeros(6)
 
-steps = emp_array.size
+# print output every 128 steps, i.e. step 128, 256, ...
 
-# print output every 128 steps
-print("Step \t E \t\t Err. E")
+print("Step \t Avg. E \t Err. E \t Avg. E Ctrled \t Err. E Ctrled")
 for i in range(127, steps, 128):
     step = i + 1
-    E = np.average(emp_array[0:i + 1])
-    err_E = np.sqrt(np.var(emp_array[0:i + 1]) / (i + 1))
+    E_avg = np.average(emp[0:i + 1])
+    var_E = np.var(emp[0:i + 1])
+    err_E = np.sqrt(var_E / step)
 
-    print(F"{step} \t {E:.7f} \t {err_E:.7f}")
+    # 6 x 1 vector of the average of all (6) control variates thus far
+    cv_avg = np.average(cv[0:i + 1], axis = 0)
+
+    E_cv_cov = np.cov(cv[0:i + 1], emp[0:i + 1], rowvar = False)[6][0:6]
+    cv_cv_cov = np.cov(cv, rowvar= False)
+    alpha = np.linalg.solve(cv_cv_cov, E_cv_cov)
+
+    E_avg_ctrl = E_avg - np.dot(alpha, cv_avg)
+    var_E_ctrl = var_E - np.dot(alpha, E_cv_cov)
+    err_E_ctrl = np.sqrt(var_E_ctrl / step) if var_E_ctrl >= 0 else -1
+
+    print(F"{step} \t {E_avg:.7f} \t {err_E:.7f} \t {E_avg_ctrl:.7f} \t {err_E_ctrl:.7f}")
