e923ff360b3ccfcbd3059291b3192aa193379a1b
Alexander <alexdor@gmail.com>
2020-03-29 17:05:46 -0500

    Fixed bug in dimer-parser

--- util/dimer_binparser.py
+++ util/dimer_binparser.py
@@ -4,40 +4,48 @@
 # MP2CV = 2, i.e. 6 control variates
 
 import numpy as np
+import re
 import sys
 
-try:
-    emp_filename = sys.argv[1]
-    cv_filename = sys.argv[2]
-except:
-    print("USAGE: dimer_binparser.py [emp binary file] [cv binary file]")
-    sys.exit()
 
-emp = np.fromfile(emp_filename)
-cv = np.fromfile(cv_filename)
-cv = np.reshape(cv, (cv.size // 6, 6))
+def load_data(fname):
+    emp_data = np.fromfile(fname)
+    cv_data = np.fromfile(re.sub('emp.bin', 'cv.bin', fname)).reshape(-1, 6)
+    return emp_data, cv_data
 
-steps = emp.size
-E_cv_cov = np.zeros(6)
 
-# print output every 128 steps, i.e. step 128, 256, ...
+def control_variate_analysis(emp, cv):
+    step = emp.size
 
-print("Step \t Avg. E \t Err. E \t Avg. E Ctrled \t Err. E Ctrled")
-for i in range(127, steps, 128):
-    step = i + 1
-    E_avg = np.average(emp[0:i + 1])
-    var_E = np.var(emp[0:i + 1])
-    err_E = np.sqrt(var_E / step)
+    E_avg = np.average(emp)
+    E_var = np.var(emp)
+    E_err = np.sqrt(E_var / step)
 
     # 6 x 1 vector of the average of all (6) control variates thus far
-    cv_avg = np.average(cv[0:i + 1], axis = 0)
+    cv_avg = np.average(cv, axis=0)
 
-    E_cv_cov = np.cov(cv[0:i + 1], emp[0:i + 1], rowvar = False)[6][0:6]
-    cv_cv_cov = np.cov(cv, rowvar= False)
+    E_cv_cov = np.cov(cv, emp[:step], rowvar=False, bias=True)[-1, :6]
+    cv_cv_cov = np.cov(cv, rowvar=False, bias=True)
     alpha = np.linalg.solve(cv_cv_cov, E_cv_cov)
 
     E_avg_ctrl = E_avg - np.dot(alpha, cv_avg)
-    var_E_ctrl = var_E - np.dot(alpha, E_cv_cov)
-    err_E_ctrl = np.sqrt(var_E_ctrl / step) if var_E_ctrl >= 0 else -1
+    E_var_ctrl = E_var - np.dot(alpha, E_cv_cov)
+    E_err_ctrl = np.sqrt(E_var_ctrl / step) if E_var_ctrl >= 0 else -1
 
-    print(F"{step} \t {E_avg:.7f} \t {err_E:.7f} \t {E_avg_ctrl:.7f} \t {err_E_ctrl:.7f}")
+    return E_avg, E_err, E_avg_ctrl, E_err_ctrl
+
+
+def main():
+    emp_data, cv_data = load_data(sys.argv[1])
+
+    print("Step \t Avg. E \t Err. E \t Avg. E Ctrled \t Err. E Ctrled")
+    for step in range(128, emp_data.size + 1, 128):
+        print("{} \t {:.7f} \t {:.7f} \t {:.7f} \t {:.7f}".format(step,
+            *control_variate_analysis(emp_data[:step], cv_data[:step])))
+
+
+if __name__ == main():
+    if len(sys.argv) != 3:
+        print("USAGE: dimer_binparser.py <emp binary file>")
+    else:
+        main()
