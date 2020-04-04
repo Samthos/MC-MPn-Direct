7c7518821add5eff9b8fc8e9af205b316e1c5906
hydrocodone <44106031+diracs-delta@users.noreply.github.com>
2020-04-04 01:12:44 -0500

    dimer_binparser: replicate MC-MP2 json output

--- util/dimer_binparser.py
+++ util/dimer_binparser.py
@@ -7,7 +7,7 @@ import numpy as np
 import os
 import os.path
 import sys
-
+import json
 
 def load_data(jobname):
     # force paths to be relative to cwd e.g. removes initial './'
@@ -43,17 +43,14 @@ def get_taskid(filename):
         return int(taskid['taskid'])
     return -1
 
-
-def control_variate_analysis(emp, cv):
+def control_variate_analysis(emp, cv, last):
     step = emp.size
 
     E_avg = np.average(emp)
     E_var = np.var(emp)
     E_err = np.sqrt(E_var / step)
 
-    # 6 x 1 vector of the average of all (6) control variates thus far
     cv_avg = np.average(cv, axis=0)
-
     E_cv_cov = np.cov(cv, emp[:step], rowvar=False, bias=True)[-1, :6]
     cv_cv_cov = np.cov(cv, rowvar=False, bias=True)
     alpha = np.linalg.solve(cv_cv_cov, E_cv_cov)
@@ -62,8 +59,27 @@ def control_variate_analysis(emp, cv):
     E_var_ctrl = E_var - np.dot(alpha, E_cv_cov)
     E_err_ctrl = np.sqrt(E_var_ctrl / step) if E_var_ctrl >= 0 else -1
 
+    if last:
+        class json_data:
+            EX = E_avg
+            EX2 = np.average(emp ** 2)
+            EC = cv_avg
+            EXC = np.average(np.multiply(cv.T, emp), axis = 1)
+            COVXC = EXC - EC * EX
+            ECC = (1 / step) * np.einsum('ij,ik->jk', cv, cv)
+            COVCC = ECC - np.outer(EC, EC)
+
+        return (E_avg, E_err, E_avg_ctrl, E_err_ctrl), json_data
+
     return E_avg, E_err, E_avg_ctrl, E_err_ctrl
 
+def to_json(json_data, taskid, jobname):
+    print("EX2: ", json_data.EX2)
+    print("EC: ", json_data.EC)
+    print("EXC: ", json_data.EXC)
+    print("COVXC: ", json_data.COVXC)
+    print("ECC: ", json_data.ECC)
+    print("COVCC: ", json_data.COVCC)
 
 def main():
     file_dict = load_data(sys.argv[1])
@@ -74,8 +90,13 @@ def main():
         print("TASKID: ", taskid)
         print("Step \t Avg. E \t Err. E \t Avg. E Ctrled \t Err. E Ctrled")
         for step in range(128, emp_data.size + 1, 128):
-            print("{} \t {:.7f} \t {:.7f} \t {:.7f} \t {:.7f}".format(step,
-                  *control_variate_analysis(emp_data[:step], cv_data[:step])))
+            last = True if (step == emp_data.size) else False
+            if last:
+                analysis, json_data = control_variate_analysis(emp_data[:step], cv_data[:step], last)
+                to_json(json_data, taskid, "JOB")
+            else:
+                analysis = control_variate_analysis(emp_data[:step], cv_data[:step], last)
+            print("{} \t {:.7f} \t {:.7f} \t {:.7f} \t {:.7f}".format(step, *analysis))
 
 
 if __name__ == "__main__":
