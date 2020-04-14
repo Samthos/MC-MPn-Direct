d44249fdaf421fa94d184de52662daeac798d74e
hydrocodone <44106031+diracs-delta@users.noreply.github.com>
2020-04-14 00:54:17 -0500

    dimer_nwgen: initial commit
    generates nwin files automagically from the xyz file of the dimer

---
+++ util/dimer_nwgen.py
@@ -0,0 +1,77 @@
+import sys
+import os
+
+
+def write_title(filename):
+    with open(filename + ".nwin", 'w') as f:
+        f.write('title "{}"\n\n'.format(filename))
+
+
+def write_geometry(filename, xyz):
+    n = int(xyz[0][:-1])
+    if(filename.endswith("monomer_a")):
+        dummy = ["x" + xyz[i] if i > n / 2 else xyz[i] for i in range(n + 1)]
+    elif(filename.endswith("monomer_b")):
+        dummy = ["x" + xyz[i] if i <= n / 2 and i != 0 else xyz[i] for i in range(n + 1)]
+    else:
+        dummy = xyz.copy()
+
+    atoms = set()
+    for line in xyz[1:]:
+        atoms.add(line.split(' ')[0])
+
+    with open(filename + ".xyz", 'w') as f:
+        f.write("".join(dummy))
+    with open(filename + ".nwin", 'a') as f:
+        f.write("geometry nocenter noautoz\n")
+        f.write("".join(["    " + line for line in dummy[1:]]))
+        f.write('end\n\n')
+
+    return atoms
+
+
+def write_basis(filename, atoms):
+    with open(filename + ".nwin", 'a') as f:
+        f.write("basis\n")
+        f.write("    * library aug-cc-pvdz\n")
+        for atom in atoms:
+            f.write("    x{} library {} aug-cc-pvdz\n".format(atom, atom.lower()))
+        f.write("end\n")
+
+
+def write_task(filename):
+    with open(filename + ".nwin", 'a') as f:
+        f.write("scf\n")
+        f.write("    thresh 1.0e-8\n")
+        f.write("end\n\n")
+        f.write("task scf")
+
+def main():
+    input_xyz = sys.argv[1]
+    output_dir = input_xyz[:-10]
+    with open(input_xyz, "r") as f:
+        xyz = f.readlines()
+        xyz = [str(line) for line in xyz]
+    filenames = (output_dir + "_dimer",
+                 output_dir + "_monomer_a",
+                 output_dir + "_monomer_b")
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    os.chdir(output_dir)
+
+    for filename in filenames:
+        write_title(filename)
+        atoms = write_geometry(filename, xyz)
+        write_basis(filename, atoms)
+        write_task(filename)
+
+    os.chdir("..")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage: dimer_nwgen.py [dimer XYZ file]")
+        print("dimer XYZ file must end in '_dimer.xyz'")
+    else:
+        main()
