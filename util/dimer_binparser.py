5cd282a3e08478f48f798c0da4378730dfd3dd69
hydrocodone <44106031+diracs-delta@users.noreply.github.com>
2020-05-01 16:32:44 -0500

    dimer_binparser: add --include-json opt

--- util/dimer_binparser.py
+++ util/dimer_binparser.py
@@ -23,7 +23,7 @@ def main(args):
 
     emp2cv = lambda s: s.rstrip(".emp.bin") + ".cv.bin"
 
-    if(args.single):
+    if args.single:
         emp_filename = args.single
         cv_filename = emp2cv(emp_filename)
 
@@ -31,32 +31,31 @@ def main(args):
         print(CV.from_trajectory_file(emp_filename, cv_filename))
         return 0
 
-    else:
-        if (args.auto_dimer):
-            # TODO: redo the code here
-            dmm_jobnames = list(range(3))
-            for filename in glob.glob("*.bin"):
-                first_word = filename.split(".")[0]
-                jobname = filename.split(".taskid_")[0]
-                if first_word.endswith("dimer"):
-                    dmm_jobnames[0] = jobname
-                elif first_word.endswith("monomer_a"):
-                    dmm_jobnames[1] = jobname
-                elif first_word.endswith("monomer_b"):
-                    dmm_jobnames[2] = jobname
+    if args.auto_dimer:
+        # TODO: redo the code here
+        dmm_jobnames = list(range(3))
+        for filename in glob.glob("*.bin"):
+            first_word = filename.split(".")[0]
+            jobname = filename.split(".taskid_")[0]
+            if first_word.endswith("dimer"):
+                dmm_jobnames[0] = jobname
+            elif first_word.endswith("monomer_a"):
+                dmm_jobnames[1] = jobname
+            elif first_word.endswith("monomer_b"):
+                dmm_jobnames[2] = jobname
 
-            print("Detected dimer jobname:     ", dmm_jobnames[0])
-            print("Detected monomer A jobname: ", dmm_jobnames[1])
-            print("Detected monomer B jobname: ", dmm_jobnames[2])
-            if input("Is this correct? [Y/n]: ") in ["n", "N"]:
-                print("Aborted per user direction.")
-                return -1
-        else:
-            dmm_jobnames = args.dimer
+        print("Detected dimer jobname:     ", dmm_jobnames[0])
+        print("Detected monomer A jobname: ", dmm_jobnames[1])
+        print("Detected monomer B jobname: ", dmm_jobnames[2])
+        if input("Is this correct? [Y/n]: ") in ["n", "N"]:
+            print("Aborted per user direction.")
+            return -1
+    else:
+        dmm_jobnames = args.dimer
 
 
     dimer_jobname = dmm_jobnames[0]
-    new_data = CV.CV()
+    data = CV.CV()
     for dimer_emp_file in glob.glob(dimer_jobname + "*" + args.extension + ".emp.bin"):
         monomer_a_emp_file = re.sub("dimer", "monomer_a", dimer_emp_file)
         monomer_b_emp_file = re.sub("dimer", "monomer_b", dimer_emp_file)
@@ -69,16 +68,19 @@ def main(args):
         cv = np.fromfile(dimer_cv_file) - np.fromfile(monomer_a_cv_file) - np.fromfile(monomer_b_cv_file)
         cv = cv.reshape(emp.size, -1)
 
-        new_data = new_data + CV.from_trajectory(emp, cv)
+        data = data + CV.from_trajectory(emp, cv)
 
+    # include json files of previous calculations, if any
+    if args.include_json:
+        for json_filename in args.include_json:
+            data = data + CV.from_json(json_filename)
+
+    # print CV object
     print_header()
-    print(new_data)
+    print(data)
 
-    # TODO: specify json files to average over
-    # old_data = sum([CV.CV.from_json(json_file) for json_file in glob.glob("*.json")])
-
+    # write summary to JSON file
     json_filename = "DIMER_ANALYSIS_" + dimer_jobname.replace("_dimer", "") + ".{0}.json".format(args.extension)
-    data = new_data
     data.to_json(json_filename)
 
     # TODO: implement args.debug
@@ -88,7 +90,10 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--debug", action = "store_true",
                         help = "run in debug mode and print calculated trajectory every 128 steps per thread. significantly slows script. (currently not implemented)")
-    parser.add_argument('-e', '--extension', default='22', help='sets extension of bin files.')
+    parser.add_argument('-e', '--extension', default = '22', help='sets extension of bin files.')
+    parser.add_argument('--include-json', nargs = '*', metavar = '[JSON FILES]',
+                        help = "specify JSON trajectories of previous calculations to include in energy calculation.")
+
     dimer_calc = parser.add_mutually_exclusive_group()
     dimer_calc.add_argument("--single", metavar = "[EMP FILE NAME]",
                             help = "process energy and control variate trajectory from a single file.")
