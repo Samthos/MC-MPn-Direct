c7ab06ac0abd310f1e3349e6b8be0fb035191c3b
Alexander <alexdor@gmail.com>
2020-08-27 16:27:36 -0500

    tiled transpose/upper_from_lower calls

--- src/blas_calls.cpp
+++ src/blas_calls.cpp
@@ -13,20 +13,88 @@ void Ddgmm(const enum DDGMM_SIDE Side,
       C, ldc, std::multiplies<>());
 }
 
-void Transpose(const double*A, int m, double *B) {
-  for (int row = 0; row < m; row++) {
-    for (int col = 0; col < m; col++) {
-      B[col * m + row] = A[row * m + col];
+void tiled_transpose(const double* A, int lda, double* B) {
+  constexpr int tile_size = 8;
+  for (int col = 0; col < tile_size; col++) {
+    for (int row = 0; row < tile_size; row++) {
+      B[row * lda + col] = A[col * lda + row];
     }
   }
 }
 
-void set_Upper_from_Lower(double *A, int m) {
-  for (int col = 0; col < m; ++col) {
-    for (int row = col+1; row < m; ++row) {
-      A[row * m + col] = A[col * m + row];
+void tiled_transpose(const double* A, int lda, double* B, int tile_size) {
+  for (int col = 0; col < tile_size; col++) {
+    for (int row = 0; row < tile_size; row++) {
+      B[row * lda + col] = A[col * lda + row];
     }
   }
+}
+
+void tiled_transpose_col(const double* A, int lda, double* B, int col_tile_size) {
+  constexpr int tile_size = 8;
+  for (int col = 0; col < col_tile_size; col++) {
+    for (int row = 0; row < tile_size; row++) {
+      B[row * lda + col] = A[col * lda + row];
+    }
+  }
+}
+
+void tiled_transpose_row(const double* A, int lda, double* B, int row_tile_size) {
+  constexpr int tile_size = 8;
+  for (int col = 0; col < tile_size; col++) {
+    for (int row = 0; row < row_tile_size; row++) {
+      B[row * lda + col] = A[col * lda + row];
+    }
+  }
+}
+
+void Transpose(const double*A, int lda, double *B) {
+  constexpr int tile_size = 8;
+  int n = lda % 8;
+  int m = lda - n;
+
+  for (int col = 0; col < m; col+=tile_size) {
+    for (int row = 0; row < m; row+=tile_size) {
+      tiled_transpose(A + col * lda + row, lda, B + row * lda + col);
+    }
+    tiled_transpose_row(A + col * lda + m, lda, B + m * lda + col, n);
+  }
+  for (int row = 0; row < m; row+=tile_size) {
+    tiled_transpose_col(A + m * lda + row, lda, B + row * lda + m, n);
+  }
+  tiled_transpose(A + m * lda + m, lda, B + m * lda + m, n);
+}
+
+void tiled_set_upper_from_lower(double* A, int lda) {
+  constexpr int tile_size = 8;
+  for (int col = 0; col < tile_size; ++col) {
+    for (int row = col+1; row < tile_size; ++row) {
+      A[row * lda + col] = A[col * lda + row];
+    }
+  }
+}
+
+void tiled_set_upper_from_lower(double* A, int lda, int tile_size) {
+  for (int col = 0; col < tile_size; ++col) {
+    for (int row = col+1; row < tile_size; ++row) {
+      A[row * lda + col] = A[col * lda + row];
+    }
+  }
+}
+
+void set_Upper_from_Lower(double *A, int lda) {
+  constexpr int tile_size = 8;
+  int n = lda % 8;
+  int m = lda - n;
+
+  for (int col = 0; col < m; col += tile_size) {
+    tiled_set_upper_from_lower(A + col * lda + col, lda);
+    for (int row = col + tile_size; row < m; row += tile_size) {
+      tiled_transpose(A + col * lda + row, lda, A + row * lda + col);
+    }
+    tiled_transpose_row(A + col * lda + m, lda, A + m * lda + col, n);
+  }
+    tiled_set_upper_from_lower(A + m * lda + m, lda, n);
 }
 
 void dspr_batched(
---
+++ test/blas_call_test/blas_calls_test.cpp
@@ -0,0 +1,42 @@
+#include <numeric>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "../../src/blas_calls.h"
+
+
+namespace {
+  TEST(blasTest, TransposeTest) {
+    constexpr int N = 19;
+    std::vector<double> A(N * N);
+    std::vector<double> B(N * N, 0.0);
+
+    std::iota(A.begin(), A.end(), 0.0);
+    Transpose(A.data(), N, B.data());
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < N; col++) {
+        ASSERT_EQ(A[row * N + col], B[col * N + row]) << "(row, col) == (" << row << ", " << col << ")";
+      }
+    }
+  }
+
+  TEST(blasTest, SetUpperFromLowerTest) {
+    constexpr int N = 19;
+    std::vector<double> A(N * N, 0);
+
+    for (int row = 0; row < N; row++) {
+      for (int col = row; col < N; col++) {
+        A[row * N + col] = row * N + col;
+      }
+    }
+
+    set_Upper_from_Lower(A.data(), N);
+
+    for (int row = 0; row < N; row++) {
+      for (int col = row; col < N; col++) {
+        ASSERT_EQ(A[row * N + col], A[col * N + row]) << "(row, col) == (" << row << ", " << col << ")";
+      }
+    }
+  }
+}
